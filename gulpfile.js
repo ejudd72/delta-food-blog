2a24259bb70adbe389d987e77af03c53654fc731
NicJr1110 <nicholasjnorton@gmail.com>
2019-10-31 11:21:00 +0000

    Update of Gulpfile to contain the HTML minify and browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ let rename = require('gulp-rename');
 let sass = require('gulp-sass');
 let concat = require('gulp-concat');
 let uglify = require('gulp-uglify-es').default;
+let htmlmin = require('gulp-htmlmin');
+let browserSync = require('browser-sync').create();
 
 let js_files = ['./src/js/jquery-3.3.1.slim.js','./src/js/popper.js','./src/js/bootstrap.js'];
 
@@ -31,7 +33,7 @@ gulp.task('watch', () =>{
     }
 );
 
-
+//concatenates the JS file and 
 gulp.task('concatJS', () => {
     return gulp.src(js_files)
       .pipe(concat('src/scripts.js'))
@@ -54,4 +56,29 @@ gulp.task("watch-js", () => {
     }
 );
 
- 
+//HTML MINIFY - destination is dist(ribution) folder
+gulp.task('minify', () => {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+
+// This is the browser sync function
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+function serve(done) {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  done();
+}
+
+const watch = () => gulp.watch(['./scss/**/*.scss', './js/*.js'], gulp.series('styles', 'scripts', reload));
+
+gulp.task('default', gulp.series(serve, watch));
