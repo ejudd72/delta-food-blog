545cb7aaf94008106dab6ac329bbdb2844a6ddb7
jae.L <jklee900414@gmail.com>
2019-10-31 12:56:12 +0000

    conflicts fixed

--- README.md
+++ README.md
@@ -4,8 +4,33 @@ Team Delta's food blog project
 ## To clone repository
  on command line use git clone git@github.com:ejudd72/delta-food-blog 
 
-## Install NPM
-On command line type 'npm install' in the project directory and this will install all relevant packages (seen in node modules folder) for the project.
 
 ## Dist and Src folders
 You should only change what is in the src file, do not touch the dist folder. This contains files automatically compiled by our css watch and js files and is minified. 
+
+
+## Install NPM Packages
+On command line type 'npm install' in the project directory and this will install all relevant packages (seen in node modules folder) for the project.
+
+### Packages required:
+1. gulp
+2. gulp-clean-css
+3. gulp-concat
+4. gulp-htmlmin
+5. gulp-rename
+6. gulp-sass
+7. gulp-uglify-es
+8. browser-sync
+
+## Work In Progress
+### Ellie
+1. Footer
+
+### Jae
+1. Navbar 
+
+### Ross
+1. Carousel 
+
+### Nic
+1. Gulpfile HTML & Browser-Sync
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ let rename = require('gulp-rename');
 let sass = require('gulp-sass');
 let concat = require('gulp-concat');
 let uglify = require('gulp-uglify-es').default;
+let htmlmin = require('gulp-htmlmin');
+let browserSync = require('browser-sync').create();
 
 let js_files = ['./src/js/jquery-3.3.1.slim.js','./src/js/popper.js','./src/js/bootstrap.js'];
 
@@ -31,14 +33,14 @@ gulp.task('watch', () =>{
     }
 );
 
-
+//concatenates the JS file and 
 gulp.task('concatJS', () => {
     return gulp.src(js_files)
       .pipe(concat('src/scripts.js'))
       .pipe(gulp.dest('./src/js/'));
     }
 );
-
+//uglifies the JS and saves in dist folder
 gulp.task('uglify', () => {
     return gulp.src('src/js/scripts.js')
         .pipe(rename('scripts.min.js'))
@@ -46,12 +48,38 @@ gulp.task('uglify', () => {
         .pipe(gulp.dest('./dist/js/'));   
     }
 );
-
+// Scripts task combines, concatenation and uglifying
 gulp.task('scripts', gulp.series('concatJS','uglify'));
 
+//watch task for the js scripts task
 gulp.task("watch-js", () => {
     return gulp.watch(js_files, gulp.series('scripts'));
     }
 );
 
- 
+//HTML MINIFY - destination is dist(ribution) folder
+gulp.task('minify-HTML', () => {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+
+// This is the browser sync function
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+function serve(done) {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+  done();
+}
+
+let watchBrowser = () => gulp.watch(['./src/scss/**/*.scss', './src/js/*.js'], gulp.series('styles', 'scripts', reload));
+
+gulp.task('default', gulp.series(serve, watchBrowser));
